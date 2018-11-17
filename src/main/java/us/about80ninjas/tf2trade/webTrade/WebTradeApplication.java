5bda038bcd2607aa9915a7ff67436b9877d2d64c
About80Ninjas <jordan.henry@outlook.com>
2018-11-17 14:14:17 -0600

    init commit

---
+++ src/main/java/us/about80ninjas/tf2trade/webTrade/WebTradeApplication.java
@@ -0,0 +1,12 @@
+package us.about80ninjas.tf2trade.webTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTradeApplication.class, args);
+	}
+}
